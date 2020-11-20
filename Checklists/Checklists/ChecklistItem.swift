2a5130daa809785e6c90f8942f81823b5d92ad9d
Rajha Fajardo <39444541+RajhaFajardo@users.noreply.github.com>
2020-11-20 09:11:16 -0300

    Add files via upload

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Rajha Fajardo de Alencar Marcal on 18/11/20.
+//
+
+import Foundation
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
